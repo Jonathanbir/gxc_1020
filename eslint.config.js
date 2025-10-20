55ee0ae42efc64e86ffc13c96c61ffc11b1d7f97
jonathanbir <jonathan79111@gmail.com>
2025-10-20 18:10:31 +0800

    git: 重新設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,18 @@
-import js from "@eslint/js";
-import pluginSecurity from "eslint-plugin-security";
+import security from "eslint-plugin-security";
 
 export default [
-  js.configs.recommended,
   {
-    plugins: {
-      security: pluginSecurity,
+    files: ["**/*.js", "**/*.ts"],
+    ignores: ["node_modules/**", "dist/**"],
+    plugins: { security },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     rules: {
-      // 啟用 security 插件建議規則
-      ...pluginSecurity.configs.recommended.rules,
+      "security/detect-object-injection": "warn",
+      "security/detect-eval-with-expression": "error",
+      "security/detect-non-literal-fs-filename": "error",
     },
-    ignores: ["node_modules/**", "dist/**"],
   },
 ];
